8f2bf20474b997542050589891a42f1c79e11769
ThataBispo <thauane05.bs@gmail.com>
2023-11-21 10:09:04 -0300

    loginfocus

--- src/pages/LoginTeacher/index.tsx
+++ src/pages/LoginTeacher/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import logoImg from "../../assets/images/logo.png";
 
 import "./styles.css";
 
 export function LoginTeacher() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="container">
       <div className="container-login">
@@ -14,12 +17,22 @@ export function LoginTeacher() {
             </span>
 
             <div className="wrap-input">
-              <input className="input" type="email" />
+              <input
+                className={email !== "" ? "has-val input" : "input"}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <span className="focus-input" data-placeholder="Email"></span>
             </div>
 
             <div className="wrap-input">
-              <input className="input" type="password" />
+              <input
+                className={password !== "" ? "has-val input" : "input"}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <span className="focus-input" data-placeholder="Password"></span>
             </div>
 
